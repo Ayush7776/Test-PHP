4d738c8eda3ec4c841b3684543c62481c7a67781
Yash Pathak <106083687+Ayush7776@users.noreply.github.com>
2023-02-13 16:25:08 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Php Demo On github</h1>
+    <?php
+
+    
+    echo"Hello In PHP <br>";
+    $age=45;
+    echo"My Age Is ",$age;
+    
+    ?>
+</body>
+</html>
